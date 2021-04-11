6cbaca193862c9289c319ac951099ad120d6b98f
Aaron Woodbury <aarondswoodbury@gmail.com>
2021-04-10 19:52:58 -0600

    after part 6 with 403 errors

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,15 +1,33 @@
 <template>
+
 <div>
-  <HomePage />
+  <MyTickets v-if="user" />
+  <HomePage v-else />
 </div>
 </template>
 
 <script>
+import axios from 'axios';
 import HomePage from '@/components/HomePage.vue'
+import MyTickets from '@/components/MyTickets.vue'
 export default {
   name: 'home',
   components: {
-    HomePage
+    HomePage,
+    MyTickets
+  },
+  async created() {
+    try {
+      let response = await axios.get('/api/users');
+      this.$root.$data.user = response.data.user;
+    } catch (error) {
+      this.$root.$data.user = null;
+    }
+  },
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    }
   }
 }
 </script>
